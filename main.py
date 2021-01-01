50ef0d55bdd991e59ffa4e7d97016ea68adaeff8
Viktor <viktor.malakhov05@gmail.com>
2021-01-01 19:30:20 +0500

    add terminate

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import pygame
+import sys
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
 
 FPS = 60
 SIZE = HEIGHT, WIDTH = 800, 600
 screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
 
-
 while True:
     screen.fill("blue")
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            exit()
-
+            terminate()
     pygame.display.flip()
     clock.tick(FPS)
