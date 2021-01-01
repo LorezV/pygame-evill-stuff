02e0353866a08a8a994f59b71f4a198106e81d83
LorezV <derkach.tashcher@bk.ru>
2021-01-01 19:53:16 +0500

    Прототип управления игроком №1

--- main.py
+++ main.py
@@ -13,11 +13,14 @@ FPS = 60
 SIZE = HEIGHT, WIDTH = 800, 600
 screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
+player = Player()
 
 while True:
     screen.fill(BLUE)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             terminate()
+
+    player.draw(screen)
     pygame.display.flip()
     clock.tick(FPS)
