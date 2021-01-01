ee69a214d54b2a28c12166d3258672e5e8cdf67e
LorezV <derkach.tashcher@bk.ru>
2021-01-01 20:12:33 +0500

    Улучшение управления игроком.

--- modules/Player.py
+++ modules/Player.py
@@ -1,4 +1,5 @@
-import pygame
+import pygame, math
+from .Settings import *
 
 
 class Player():
@@ -6,17 +7,37 @@ class Player():
         super().__init__()
         self.x, self.y = 100, 100
 
+        # Перенести в settings
+        self.player_speed = 2
+        self.angle = 0
+
+    @property
+    def pos(self):
+        return (self.x, self.y)
+
     def draw(self, surface):
         pygame.draw.circle(surface, (150, 150, 255), (self.x, self.y), 10)
+        pygame.draw.line(surface, YELLOW, self.pos,
+                         (self.x + 20 * math.cos(self.angle), self.y + 20 * math.sin(self.angle)), 2)
         self.movement()
 
     def movement(self):
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
         keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.x += self.player_speed * cos_a
+            self.y += self.player_speed * sin_a
+        if keys[pygame.K_s]:
+            self.x += -self.player_speed * cos_a
+            self.y += -self.player_speed * sin_a
+        if keys[pygame.K_a]:
+            self.x += self.player_speed * sin_a
+            self.y += -self.player_speed * cos_a
+        if keys[pygame.K_d]:
+            self.x += -self.player_speed * sin_a
+            self.y += self.player_speed * cos_a
+        if keys[pygame.K_LEFT]:
+            self.angle -= 0.02
         if keys[pygame.K_RIGHT]:
-            self.x += 10
-        elif keys[pygame.K_LEFT]:
-            self.x -= 10
-        if keys[pygame.K_UP]:
-            self.y -= 10
-        if keys[pygame.K_DOWN]:
-            self.y += 10
+            self.angle += 0.02
