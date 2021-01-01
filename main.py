78ae851d86ec4b1b7f6566bcb8f9f60f202b8d30
LorezV <derkach.tashcher@bk.ru>
2021-01-01 19:12:47 +0500

    Test

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import pygame
+
+FPS = 60
+SIZE = HEIGHT, WIDTH = 800, 600
+screen = pygame.display.set_mode(SIZE)
+clock = pygame.time.Clock()
+
+
+while True:
+    screen.fill("blue")
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    pygame.display.flip()
+    clock.tick(FPS)
