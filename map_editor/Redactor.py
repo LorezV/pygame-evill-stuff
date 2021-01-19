eaa4ddefc49d06e769f3e09b0e8e1cd7e2e42e3a
Viktor <viktor.malakhov05@gmail.com>
2021-01-19 21:39:45 +0500

    updrade map redactor

--- map_editor/Redactor.py
+++ map_editor/Redactor.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QInputDialog
 from PyQt5.QtGui import QColor
 import sys
 
@@ -14,57 +14,94 @@ class Level_maker(QMainWindow):
         self.w_btn.clicked.connect(self.change_color)
 
     def setupUI(self):
-        self.setGeometry(500, 100, 900, 950)
+        self.setGeometry(500, 100, 1200, 800)
         self.btns = []
         for i in range(0, 600, 15):
+            row = []
             for j in range(0, 900, 15):
                 btn = QPushButton(self)
                 btn.resize(15, 15)
-                btn.move(100 + i, j)
+                btn.move(100 + j, 100 + i)
                 btn.clicked.connect(self.colorise)
                 btn.setStyleSheet(f"background-color: {self.colors[1].name()}")
-                self.btns.append(btn)
+                row.append(btn)
+            self.btns.append(row)
         self.r_btn = QPushButton(self)
-        self.r_btn.move(750, 400)
-        self.r_btn.setText('RED')
+        self.r_btn.move(1000, 400)
+        self.r_btn.setText('Клетка стены')
+        self.r_btn.resize(180, 40)
         self.b_btn = QPushButton(self)
-        self.b_btn.move(750, 450)
-        self.b_btn.setText('BLACK')
+        self.b_btn.move(1000, 450)
+        self.b_btn.setText('Свободная клетка')
+        self.b_btn.resize(180, 40)
         self.w_btn = QPushButton(self)
-        self.w_btn.move(750, 500)
-        self.w_btn.setText('WHITE')
+        self.w_btn.move(1000, 500)
+        self.w_btn.setText('Дополнительные текстуры')
+        self.w_btn.resize(180, 40)
         self.save_btn = QPushButton(self)
         self.save_btn.setText('Сохранить карту')
-        self.save_btn.move(750, 800)
+        self.save_btn.move(1000, 700)
+        self.save_btn.resize(180, 40)
         self.save_btn.clicked.connect(self.saves)
 
     def change_color(self):
-        if self.sender().text() == 'RED':
+        if self.sender().text() == 'Клетка стены':
             self.current_color = 0
-        elif self.sender().text() == 'BLACK':
+        elif self.sender().text() == 'Свободная клетка':
             self.current_color = 1
-        elif self.sender().text() == 'WHITE':
+        elif self.sender().text() == 'Дополнительные текстуры':
             self.current_color = 2
 
     def colorise(self):
         self.sender().setStyleSheet(f"background-color: {self.colors[self.current_color].name()}")
-        print(self.sender().styleSheet())
 
     def saves(self):
-        f = open('second_lvl.txt', encoding='utf-8', mode='w')
-        row = ''
-        for i in range(len(self.btns)):
-            if self.btns[i].styleSheet() == 'background-color: #000000':
-                row += '0'
-            if self.btns[i].styleSheet() == 'background-color: #ffffff':
-                row += '9'
-            if self.btns[i].styleSheet() == 'background-color: #ff0000':
-                row += '5'
-            if (i + 1) % 60 == 0:
-                row += '\n'
-                f.write(row)
-                row = ''
+        name = self.run()
+        if not name:
+            return
+        try:
+            f = open(f'{name}.txt', encoding='utf-8', mode='w')
+        except Exception as ex:
+            self.statusBar().showMessage('Неверное имя файла!')
+            return
+        free, walls, another = self.check()
+        for rows in self.btns:
+            row = ''
+            for btn in rows:
+                if btn.styleSheet() == 'background-color: #000000':
+                    row += free
+                if btn.styleSheet() == 'background-color: #ffffff':
+                    row += another
+                if btn.styleSheet() == 'background-color: #ff0000':
+                    row += walls
+            print(row)
+            row += '\n'
+            f.write(row)
         f.close()
+
+    def run(self):
+        name, ok_pressed = QInputDialog.getText(self, "Введите название",
+                                                "Как вы назовёте файл?")
+        if ok_pressed:
+            return name
+
+    def check(self):
+        free, ok_pressed1 = QInputDialog.getInt(
+            self, "Введите кодировку пустой клетки", "Кодировка пустой клетки",
+            0, 0, 9, 1)
+        wall, ok_pressed2 = QInputDialog.getInt(
+            self, "Введите кодировку клетки стены", "Кодировка стены",
+            5, 0, 9, 1)
+        another, ok_pressed3 = QInputDialog.getInt(
+            self, "Введите кодировку дополнительной текстуры", "Кодировка дополнительной текстуры",
+            9, 0, 9, 1)
+        if not ok_pressed1:
+            free = '0'
+        if not ok_pressed2:
+            wall = '5'
+        if not ok_pressed3:
+            another = '9'
+        return str(free), str(wall), str(another)
 
 
 if __name__ == '__main__':
